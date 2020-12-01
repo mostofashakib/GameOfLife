bc793a1d6ea5e4133b59f178208e9d85b2669f6b
adibshakib <adibshakib@gmail.com>
2020-12-01 00:32:25 -0600

    commented my code

--- Multi Processing/Mostofa_Shakib_R11545508_final_project.py
+++ Multi Processing/Mostofa_Shakib_R11545508_final_project.py
@@ -1,8 +1,22 @@
+"""
+  Author: Mostofa Adib Shakib
+  Date: 12/01/2020
+  Language: Python
+  Description:
+   A multithreaded program capable of executing the first 100 steps of a modified cellular life simulator.
+   This simulator receives the path to the input file as an argument containing the starting cellular matrix.
+   The program then simulates the next 100 time-steps based on the algorithm defined.
+"""
+
+# Importing the necessary libraries
+
 import sys, os
 from utils import *
 from multiprocessing import Pool
 
 def main():
+
+	# Reads all the commend line arguments that are passed
 
     n = len(sys.argv)
 
@@ -14,7 +28,11 @@ def main():
     if sys.argv[3] == "-o":
     	outputFileDestination = sys.argv[4]
 
+    # If -t is not passed then the default value for the number of thread is 1
+
     numberOfThreads = 1
+
+    # If -t is passed we parse the thread count from the command line argument
 
     if n > 5 and sys.argv[5] == "-t":
     	numberOfThreads = int(sys.argv[6])
@@ -23,36 +41,44 @@ def main():
 
     board = []
 
+    # Reads the input file line by line and initializes them to a 2D matrix as the starting cellular matrix
+
     for line in Lines:
     	board.append(list(line.strip()))
 
-    rows = len(board)
-    columns = len(board[0])
+    rows = len(board)					# Calculates the number of rows in the matrix
+    columns = len(board[0])				# Calculates the number of columns in the matrix
     aliveCells = 0
 
     print("Project :: R11545508\n")
 
+    # Initializing the multiprocessing object
+
     processPool = Pool(processes=numberOfThreads)
-    
+
     for i in range(100):
+    	# Make the processed matrix the initial matrix for the ith iteration
     	if i > 0:
     		board = result
 
     	poolData = []
 
     	dividend = rows//numberOfThreads
-    	extras = rows % numberOfThreads
+    	extraThreads = rows % numberOfThreads
     	count = numberOfThreads
     	startRow = 0
     	endRow = startRow + dividend
 
+    	# Break down the original matrix into submatrices where each worker takes 'dividend' number of rows
+    	# If there are extra threads then we give each workers one more row to process in an attempt to divide the work as equally as possible
+
     	while(count > 1):
-    		if extras > 0:
+    		if extraThreads > 0:
     			poolData.append([board, startRow, endRow+1])
     			startRow = endRow+1
     			endRow = startRow + dividend
     			count -= 1
-    			extras -= 1
+    			extraThreads -= 1
     		else:
     			poolData.append([board, startRow, endRow])
     			startRow = endRow
@@ -61,14 +87,20 @@ def main():
 
     	poolData.append([board, startRow, rows])
 
+    	# This processes individual submatrices using the multiprocessing library
+
     	finalData = processPool.map(processMatrix, poolData)
 
-    	del (poolData)
+    	del (poolData)	# Delete the poolData in order to save space
+
+    	# We need to convert the 3D matrix to a 2D matrix which will be used as the starting matrix for the next iteration
 
     	result = finalData[0]
 
     	for i in range(1, len(finalData)):
     		result += finalData[i]
+
+    # This writes the 100th iteration to an output file
 
     with open(outputFileDestination, "w") as fp:
     	for row in range(rows):
--- Multi Processing/utils.py
+++ Multi Processing/utils.py
@@ -1,42 +1,56 @@
 __name__ = 'utils'
 
+# A helper function that checks how many neighbouring alive cells does a particular cell have
 def checkNeighbours(board, row, column):
     cells = 0
     
+    # Check the left cell
     if board[row][ (column-1) % len(board[0]) ] == 'O':
         cells += 1
     
+    # Check the right cell
     if board[row][ (column+1) % len(board[0]) ] == 'O':
         cells += 1
-        
+    
+    # Check the top cell
     if board[ (row-1) % len(board) ][column] == 'O':
         cells += 1
-    
+
+    # Check the bottom cell
     if board[ (row+1) % len(board) ][column] == 'O':
         cells += 1
     
+    # Check the bottom-right cell
     if board[ (row+1) % len(board) ][ (column+1) % len(board[0]) ] == 'O':
         cells += 1
-        
+    
+    # Check the bottom-left cell
     if board[ (row+1) % len(board) ][ (column-1) % len(board[0]) ] == 'O':
         cells += 1
-        
+    
+    #  Check the top-right cell
     if board[ (row-1) % len(board) ][ (column+1) % len(board[0]) ] == 'O':
         cells += 1
-        
+
+    # Check the top-left cell
     if board[ (row-1) % len(board) ][ (column-1) % len(board[0]) ] == 'O':
         cells += 1
         
     return cells
 
+# A helper function that takes a submatrix and processes it based on the predefined algorithm
 def processMatrix(matrixData):
     board = matrixData[0]
     startRow = matrixData[1]
     endRow = matrixData[2]
     columnLength = len(board[0])
 
+    # Initializing the submatrix for the next board
+
     difference = endRow - startRow
     nextBoard = [ [0 for col in range(columnLength)] for row in range(difference) ]
+
+    # We iterate over each cell in the submatrix and decide if a given cell will be alive/dead based on the predefinded algorithm
 
     for row in range(difference):
         for column in range(columnLength):
